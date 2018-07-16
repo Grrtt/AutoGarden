b8d7aed207e319a73d4236efba636ccb760bbf80
Garrett Shepherd <shepherd.garrett@gmail.com>
2018-07-15 20:01:43 -0400

    Implement Pin Direction Reader

---
+++ RaspPi.Tests/PinIO/TestGpioPinDirectionReader.cs
@@ -0,0 +1,74 @@
+﻿namespace RaspPi.Tests.PinIO
+{
+    using System.IO;
+
+    using NUnit.Framework;
+
+    using RaspPi.Abstractions;
+    using RaspPi.PinIO;
+
+    [TestFixture]
+    public class TestGpioPinDirectionReader
+    {
+        private const string GpioPinDirectory = "/sys/class/gpio/gpio1";
+
+        private GpioPinDirectionReader systemUnderTest;
+
+        [Test]
+        public void GetPinDirection_WhenDirectionValueIsIn_ReturnsGpioPinDirectionIn()
+        {
+            WriteValueToGpioPinDirectionFile("in");
+
+            GpioPinDirection actual = InvokeGetPinDirection(GpioPin.One);
+
+            Assert.That(actual, Is.EqualTo(GpioPinDirection.In));
+        }
+
+        [Test]
+        public void GetPinDirection_WhenDirectionValueIsOut_ReturnsGpioPinDirectionOut()
+        {
+            WriteValueToGpioPinDirectionFile("out");
+
+            GpioPinDirection actual = InvokeGetPinDirection(GpioPin.One);
+
+            Assert.That(actual, Is.EqualTo(GpioPinDirection.Out));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CreateGpioDirectory();
+
+            systemUnderTest = CreateSystemUnderTest();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (Directory.Exists("/sys"))
+            {
+                Directory.Delete("/sys/", true);
+            }
+        }
+
+        private void CreateGpioDirectory()
+        {
+            Directory.CreateDirectory(GpioPinDirectory);
+        }
+
+        private GpioPinDirectionReader CreateSystemUnderTest()
+        {
+            return new GpioPinDirectionReader();
+        }
+
+        private GpioPinDirection InvokeGetPinDirection(GpioPin pin)
+        {
+            return systemUnderTest.GetPinDirection(pin);
+        }
+
+        private void WriteValueToGpioPinDirectionFile(string value)
+        {
+            File.WriteAllLines($"{GpioPinDirectory}/direction", new[] { value });
+        }
+    }
+}
--- RaspPi/PinIO/GpioPinDirectionReader.cs
+++ RaspPi/PinIO/GpioPinDirectionReader.cs
@@ -1,13 +1,38 @@
 ﻿namespace RaspPi.PinIO
 {
+    using System.Collections.Generic;
+    using System.IO;
+
     using RaspPi.Abstractions;
 
     public class GpioPinDirectionReader : IGpioPinDirectionReader
     {
+        private readonly IDictionary<string, GpioPinDirection> directions;
+
+        public GpioPinDirectionReader()
+        {
+            directions = CreateDirectionsDictionary();
+        }
+
         public GpioPinDirection GetPinDirection(GpioPin pin)
         {
-            //TODO: To be implemented later.
-            return GpioPinDirection.In;
+            string[] lines = File.ReadAllLines($"/sys/class/gpio/gpio{(int)pin}/direction");
+            return GetDirectionStringEnumValue(lines[0]);
+        }
+
+        private IDictionary<string, GpioPinDirection> CreateDirectionsDictionary()
+        {
+            Dictionary<string, GpioPinDirection> dictionary = new Dictionary<string, GpioPinDirection>();
+
+            dictionary.Add("in", GpioPinDirection.In);
+            dictionary.Add("out", GpioPinDirection.Out);
+
+            return dictionary;
+        }
+
+        private GpioPinDirection GetDirectionStringEnumValue(string value)
+        {
+            return directions[value];
         }
     }
 }
